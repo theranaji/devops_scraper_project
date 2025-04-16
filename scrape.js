770cd46ec2d4673fdc525d1794432319f00c2835
Vishal Kumar <vishaltheranaji@gmail.com>
2025-04-16 16:06:59 +0530

    Added updated Puppeteer version

--- scrape.js
+++ scrape.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const url = process.env.SCRAPE_URL || 'https://example.com';
+  const url = process.env.SCRAPE_URL || 'https://scrape_devopstest.com';
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
